8e49da1cc6c0359d712af8399f95328c113e0e0e
DennisLoska <d.loska@hotmail.de>
2017-07-10 19:17:52 +0200

    correcting main using Tonys example

--- src/scrabble/Main.java
+++ src/scrabble/Main.java
@@ -2,7 +2,6 @@ package scrabble;
 
 import scrabble.data.SimpleWordList;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  * Created by dennis on 10.07.2017.
@@ -11,17 +10,14 @@ public class Main {
     public static void main(String[] args) {
         boolean stay = true;
         SimpleWordList list = new SimpleWordList();
+        list.initFromFile("wordlists/sowpods.txt");
         System.out.println("Willkommen zum Scrabble-Cheater!");
         do {
             System.out.println("Bitte gib die Buchstaben ein, um Lösungen zu finden:");
             Scanner sc = new Scanner(System.in);
             String word = sc.next();
-            list.add(word);
             System.out.println("Antwort:");
-            Set<String> set = list.validWordsUsingAllTiles(word);
-            for (String s : set) {
-                System.out.println(s);
-            }
+            System.out.println(list.validWordsUsingAllTiles(word).toString());
             System.out.println("Möchten Sie ein weiteres Wort eingeben (y|n)?");
             if (sc.next().equals("n"))
                 stay = false;
